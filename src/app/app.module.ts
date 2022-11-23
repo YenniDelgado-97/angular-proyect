c9f1ecb96692127acbe4899af5555fe27e7d5246
Yenni Vanessa Delgado <yennivanessadelgado@gmail.com>
2022-11-22 21:01:00 -0500

    this is my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroImagesComponent } from './components/hero-images/hero-images.component';
+import { BenefitsComponent } from './components/benefits/benefits.component';
+import { FeatureComponent } from './components/feature/feature.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FooterComponent } from './components/footer/footer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroImagesComponent,
+    BenefitsComponent,
+    FeatureComponent,
+    MenuComponent,
+    FooterComponent,
+  
+    
   ],
   imports: [
     BrowserModule,
